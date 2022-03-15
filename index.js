17cb594cbee0f733912c0dcedcb758666168c421
aylin <aylin.mrLarki@gmail.com>
2022-03-15 02:47:51 -0700

    new index

---
+++ index.js
@@ -0,0 +1,9 @@
+hungry = input("are you hungry");
+if (hungry) {
+  print("eat pizza");
+  print("eat food");
+  print("aet sambuse");
+  print("aet burger");
+} else {
+  print("do your homework");
+}
