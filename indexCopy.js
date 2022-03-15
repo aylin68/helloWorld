0cc91985bc090b18e258c8a1d89df14cc0e7cf88
aylin <aylin.mrLarki@gmail.com>
2022-03-15 02:43:55 -0700

    add some code

---
+++ indexCopy.js
@@ -0,0 +1,10 @@
+hungry = input("are you hungry");
+if (hungry) {
+  print("eat pizza");
+  print("eat food");
+  print("aet sambuse");
+  print("aet burger");
+} else {
+  tersty = input("are you tersty");
+  print("drink water");
+}
